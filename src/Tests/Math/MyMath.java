75f466a3ac7e36608480f9245528d6a82f37d146
extendslcc <lucas.lcc@hotmail.com>
2021-11-28 20:37:27 -0300

    :heavy_check_mark: PASS TESTs: implements div method

--- src/Tests/Math/MyMath.java
+++ src/Tests/Math/MyMath.java
@@ -14,6 +14,10 @@ public class MyMath {
         return Math.multiplyExact(a, b);
     }
 
+    public int div(int a, int b) {
+        return a / b;
+    }
+
     public static void main(String[] args) {
         MyMath math = new MyMath();
         int sumResult = math.sum(2, 2);
